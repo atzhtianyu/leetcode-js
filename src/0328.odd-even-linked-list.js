4fd86dae549c7a10b4009f66a5a4c4d770460111
zhtianyu <1420098499@qq.com>
2021-07-04 17:56:13 +0800

    奇偶链表

---
+++ src/0328.odd-even-linked-list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+    if (!head) {
+        return head;
+    }
+    const oddHead = head;
+    const evenHead = head.next;
+    let oddNode = oddHead;
+    let evenNode = evenHead;
+    while (oddNode.next && evenNode.next) {
+        oddNode.next = oddNode.next.next;
+        oddNode = oddNode.next;
+        evenNode.next = evenNode.next.next;
+        evenNode = evenNode.next;
+    }
+    oddNode.next = evenHead;
+    return oddHead;
+};
