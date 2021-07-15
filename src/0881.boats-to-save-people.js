957ab8e2bd773d46b9b6f4c39af91346321d0cdf
zhtianyu <1420098499@qq.com>
2021-07-15 09:13:53 +0800

    救生艇

---
+++ src/0881.boats-to-save-people.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function (people, limit) {
+    people.sort((a, b) => a - b);
+    let left = 0;
+    let right = people.length - 1;
+    let count = 0;
+    // 小于等于是处理最后还剩一个的情况，无论如何都要加一
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            count += 1;
+            left += 1;
+            right -= 1;
+        } else {
+            count += 1;
+            right -= 1;
+        }
+    }
+    return count;
+};
