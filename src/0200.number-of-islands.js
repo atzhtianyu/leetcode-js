cac4fa92bc3a168d899d8ccc3cec11ac7669d808
zhtianyu <1420098499@qq.com>
2021-07-25 11:12:44 +0800

    update

--- src/0200.number-of-islands.js
+++ src/0200.number-of-islands.js
@@ -2,6 +2,7 @@
  * @param {character[][]} grid
  * @return {number}
  */
+// dfs
 var numIslands = function (grid) {
     // 遍历二维数组，每当遇到1开启搜索模式，从当前节点向左/右/上/下，每次分别移动一步，如果是1则替换为0
 
@@ -29,6 +30,49 @@ var numIslands = function (grid) {
             if (grid[i][j] === "1") {
                 dfs(grid, i, j);
                 count++;
+            }
+        }
+    }
+    return count;
+};
+
+// bfs
+var numIslands = function (grid) {
+    function bfs(queue, grid) {
+        const dirs = [
+            [0, 1],
+            [1, 0],
+            [0, -1],
+            [-1, 0],
+        ];
+        while (queue.length) {
+            const cur = queue.shift();
+            for (const dir of dirs) {
+                const x = cur[0] + dir[0];
+                const y = cur[1] + dir[1];
+                if (
+                    x < 0 ||
+                    x >= grid.length ||
+                    y < 0 ||
+                    y >= grid[0].length ||
+                    grid[x][y] !== "1"
+                ) {
+                    continue;
+                }
+                grid[x][y] = '0';
+                queue.push([x, y]);
+            }
+        }
+    }
+    let count = 0;
+    const queue = [];
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === '1') {
+                count++;
+                grid[i][j] = '0';
+                queue.push([i, j]);
+                bfs(queue, grid);
             }
         }
     }
