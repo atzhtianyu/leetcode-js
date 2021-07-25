9082ac28c07161174fe2c2b9197f38adf0f6cf2a
zhtianyu <1420098499@qq.com>
2021-07-25 10:01:27 +0800

    update

--- src/0102.binary-tree-level-order-traversal.js
+++ src/0102.binary-tree-level-order-traversal.js
@@ -10,12 +10,13 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// 第一次思路清晰的写出来，然后提交成功，还有部分可以优化，比如temp数组。
 var levelOrder = function (root) {
     if (root === null) return [];
     let queue = [];
     let res = [];
     queue.push(root);
-    while (queue.length > 0) {
+    while (queue.length !== 0) {
         let temp = [],
             ans = [];
         temp = [...queue];
@@ -26,6 +27,25 @@ var levelOrder = function (root) {
             node.right !== null && queue.push(node.right);
         });
         res.push(ans);
+    }
+    return res;
+};
+
+// 官方题解，记录 currentLevelSize，在获取当前层时，先 push 一个空数组
+var levelOrder = function (root) {
+    const ret = [];
+    if (root === null) return ret;
+    const queue = [];
+    queue.push(root);
+    while (queue.length !== 0) {
+        const currentLevelSize = queue.length;
+        ret.push([]);
+        for (let i = 1; i <= currentLevelSize; ++i) {
+            const node = queue.shift();
+            ret[ret.length - 1].push(node.val);
+            node.left !== null && queue.push(node.left);
+            node.right !== null && queue.push(node.right);
+        }
     }
     return res;
 };
