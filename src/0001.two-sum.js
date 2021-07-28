2c8b6e40a43de067167fa1fe640c6a58819ed801
zhtianyu <1420098499@qq.com>
2021-07-28 15:57:24 +0800

    updeta

--- src/0001.two-sum.js
+++ src/0001.two-sum.js
@@ -3,6 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
+// 暴力遍历
 var twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
@@ -13,3 +14,16 @@ var twoSum = function (nums, target) {
     }
     return [];
 };
+
+// map
+var twoSum = function (nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i])) {
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
+        }
+    }
+    return [];
+}
