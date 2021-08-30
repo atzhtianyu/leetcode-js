fb6588564b424072558a4049adc43981b3713246
zhtianyu <1420098499@qq.com>
2021-08-30 11:23:03 +0800

    二叉树的镜像

---
+++ src/剑指offer27.er-cha-shu-de-jing-xiang-lcof.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var mirrorTree = function (root) {
+    if (root === null) {
+        return null;
+    }
+    const left = mirrorTree(root.left);
+    const right = mirrorTree(root.right);
+    root.right = left;
+    root.left = right;
+    return root;
+};
