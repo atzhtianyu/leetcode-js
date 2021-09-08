d821c976328fe214720b6cc727c06ac7cf90eefa
zhtianyu <1420098499@qq.com>
2021-09-08 09:16:26 +0800

    合并两个排序的链表

---
+++ src/剑指offer25.he-bing-liang-ge-pai-xu-de-lian-biao-lcof.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+    let newHead = new ListNode(), cur = newHead;
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            cur.next = l1;
+            l1 = l1.next;
+        } else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+        cur = cur.next;
+    }
+    cur.next = l1 !== null ? l1 : l2;
+    return newHead.next;
+};
